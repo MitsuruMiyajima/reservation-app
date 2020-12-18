c8b438354e4249179bcacbfc7c3dde729bc9930e
宮島充 <mitsuru@miyajimamitsurunoMacBook-Air.local>
2020-12-18 17:59:18 +0900

    フロントエンド側作成

---
+++ src/app/product/product.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+// ===== declarationsで宣言したコンポーネントをインポートする =====
+
+import { ProductComponent } from './product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  {
+  path: 'products', component: ProductComponent,
+    children: [
+      { path: '', component: ProductListComponent},
+      { path: ':productId', component: ProductDetailComponent}
+    ]
+  }
+
+
+];
+@NgModule({
+
+  // ===== declarationsにコンポーネント名を入れることでこのモジュール紐づいているコンポーネントを宣言する =====
+
+  declarations: [
+    ProductComponent,
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ProductModule { }
