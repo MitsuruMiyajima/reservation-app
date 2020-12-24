dccbdf426910200e297165ce52e2ee88b986a942
宮島充 <mitsuru@miyajimamitsurunoMacBook-Air.local>
2020-12-24 23:12:54 +0900

    nodeサーバー構築

---
+++ server/fake-db.js
@@ -0,0 +1,85 @@
+const Product = require('./model/product')
+
+class FakeDb {
+
+  constructor() {
+    this.products = [
+      {
+        coverImage: './assets/img/apple-1867461_1920.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, officiis.<',
+        headingtext2: 'テキスト　テキスト　テキスト　テキスト　テキスト　テキスト　テキスト　',
+        headingtext3: 'テキスト　テキスト　テキスト　テキスト　テキスト　テキスト　テキスト　',
+
+      },
+      {
+        coverImage: './assets/img/apple-1867461_1920.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, officiis.',
+        headingtext2: 'assumenda quibusdam dicta perspiciatis porro eveniet unde est aspernatur. Quo, minima.',
+        headingtext3: 'at et ipsam incidunt beatae! Enim facilis quae maxime optio! Numquam, corporis voluptatem!',
+
+      },
+      {
+        coverImage: './assets/img/apple-1867461_1920.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, officiis.',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, officiis.<',
+        headingtext2: 'assumenda quibusdam dicta perspiciatis porro eveniet unde est aspernatur. Quo, minima.',
+        headingtext3: 'at et ipsam incidunt beatae! Enim facilis quae maxime optio! Numquam, corporis voluptatem!',
+
+      },
+      {
+        coverImage: './assets/img/apple-1867461_1920.jpg',
+        name: 'Phone Special',
+        price: 999,
+        description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, officiis.',
+        heading1: 'サンプルテキスト1',
+        heading2: 'サンプルテキスト2',
+        heading3: 'サンプルテキスト3',
+        headingtext1: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, officiis.<',
+        headingtext2: 'assumenda quibusdam dicta perspiciatis porro eveniet unde est aspernatur. Quo, minima.@@2',
+        headingtext3: 'at et ipsam incidunt beatae! Enim facilis quae maxime optio! Numquam, corporis voluptatem!',
+
+      }
+    ]
+  }
+
+  async initDb() {
+    await this.cleanDb()
+    this.pushProductsToDb()
+  }
+
+  async cleanDb() {
+    await Product.deleteMany({})
+  }
+
+  pushProductsToDb() {
+    this.products.forEach(
+      (product) => {
+        const newProduct = new Product(product)
+        newProduct.save()
+      }
+    )
+  }
+
+  // seeDb() {
+  //   this.pushProductsToDb();
+  // }
+}
+
+module.exports = FakeDb
